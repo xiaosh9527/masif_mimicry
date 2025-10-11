2eacc469c594267fa46a7c61a4e25a4f3374ea36
annadiarov <annadiarov@gmail.com>
2025-10-11 20:51:27 +0200

    refactor(af_process): Change paths
    Change masif_root with the path inside the conteiner and use masif_seed_mimicry for dirname

--- scripts/process_af_model.sh
+++ scripts/process_af_model.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-source /work/lpdi/users/shxiao/masif_seed/load_masif_environment_jed.sh
-
-masif_root=/work/lpdi/users/shxiao/masif_seed
-masif_mimicry_root=$masif_root/masif_mimicry
+# USER CHANGEABLE PATH
+masif_root=/workspace # Assuming you're running the code in the docker container
+# DO NOT CHANGE BELOW
+masif_mimicry_root=$masif_root/masif_seed_mimicry
 masif_mimicry_script=$masif_mimicry_root/scripts
 
 export masif_mimicry_script

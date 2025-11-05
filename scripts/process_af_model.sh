5207cb9dd10d251ba0e3ac752b256caecfcbd2dd
annadiarov <annadiarov@gmail.com>
2025-11-05 14:05:07 +0100

    fix: Update old masif_seed_mimicry path to masif_mimicry

--- scripts/process_af_model.sh
+++ scripts/process_af_model.sh
@@ -3,7 +3,7 @@
 # USER CHANGEABLE PATH
 masif_root=/workspace # Assuming you're running the code in the docker container
 # DO NOT CHANGE BELOW
-masif_mimicry_root=$masif_root/masif_seed_mimicry
+masif_mimicry_root=$masif_root/masif_mimicry
 masif_mimicry_script=$masif_mimicry_root/scripts
 
 export masif_mimicry_script
